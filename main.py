736fbabaeec8d3a2249b108b3b7c92767e3e56ff
Nate BeDell <nabedell@liberty.edu>
2015-03-10 14:15:13 -0400

    Added handler code, changed the UI a bit, and most importantly: Nyan Cat loading bar.

--- main.py
+++ main.py
@@ -1,22 +1,121 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk, GdkPixbuf
 
-class Handler:
+class Application:
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("Beautiftp.glade")
+        self.builder.connect_signals(self)
+        
+        #### Application Windows
+        self.mainWindow         = self.builder.get_object("Main Window")
+        self.loadingScreen      = self.builder.get_object("Loading Screen")
+        self.permissionChange   = self.builder.get_object("Permission Change")
+        self.filechooserdialog1 = self.builder.get_object("filechooserdialog1")
+        self.openingWindow      = self.builder.get_object("Opening Window")
+
+        #### Application Objects
+        ## Permission Change Window
+        self.owner_write   = self.builder.get_object("Owner Write")
+        self.owner_read    = self.builder.get_object("Owner Read")
+        self.owner_execute = self.builder.get_object("Owner Execute")
+
+        self.group_write   = self.builder.get_object("Group Write")
+        self.group_read    = self.builder.get_object("Group Read")
+        self.group_execute = self.builder.get_object("Group Execute")
+
+        self.public_write   = self.builder.get_object("Public Write")
+        self.public_read    = self.builder.get_object("Public Read")
+        self.public_execute = self.builder.get_object("Public Execute")
+
+        self.recurse_subdirectories = self.builder.get_object("recurse subdirectories")
+
+        ## Loading Window
+        self.canvas = self.builder.get_object("Canvas")
+        self.loader=GdkPixbuf.PixbufAnimation.new_from_file("nyan2.gif")
+        self.canvas.set_from_animation(self.loader)
+
+        #
+        self.apply_all        = self.builder.get_object("Apply All")
+        self.files_only       = self.builder.get_object("Files Only")
+        self.directories_only = self.builder.get_object("Directories Only")
+        self.null_select      = self.builder.get_object("Null Select")
+        self.recurse_buttons  = [self.apply_all,self.files_only,self.directories_only,self.null_select]
+        
+        #### Application State
+
+        self.openingWindow.show_all()
+
+#### Opening Window Handler
+    def openApp(self,widget):
+        self.openingWindow.hide()
+        self.mainWindow.show_all()
+
+#### Main window handlers
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
 
     def connectHandler(self, button):
         print("Hello World!")
 
+    def disconnectHandler(self,x):
+        
+        self.openLoading(x)
+
+    def deleteHandler(self,x):
+        pass
+
+    def downloadHandler(self,x):
+        pass
+
+    def uploadHandler(self,x):
+        self.filechooserdialog1.connect('delete-event', lambda w, e: w.hide() or True)
+        self.filechooserdialog1.show_all()
+    def permissionsHandler(self,x):
+        self.permissionChange.connect('delete-event', lambda w, e: w.hide() or True)
+        self.permissionChange.show_all()
+
+    def directoryHandler(self,x):
+        pass
+
     def openLoading(self, button):
-        loadingScreen.show_all()
+        self.loadingScreen.connect('delete-event', lambda w, e: w.hide() or True)
+        self.loadingScreen.show_all()
 
-builder = Gtk.Builder()
-builder.add_from_file("Beautiftp.glade")
+#### Loading Window Handlers
+    def LDCancelHandler(self,x):
+        print("test")
 
-mainWindow = builder.get_object("Main Window")
-loadingScreen = builder.get_object("Loading Screen")
+    def LDOkHandler(self,x):
+        pass
 
-builder.connect_signals(Handler())
-mainWindow.show_all()
+#### File Chooser Window Handlers
+    def FCCancelHandler(self,x):
+        pass
+    def FCOkHandler(self,x):
+        pass
 
-Gtk.main()
+#### Permission Change Window Handlers
+    def PCCancelHandler(self,x):
+        pass
+
+    def PCOKHandler(self,x):
+        print(self.public_read.get_active())
+
+    def PCRecurseSubdirectoriesToggle(self,widget):
+        if(self.recurse_subdirectories.get_active()):
+            for x in self.recurse_buttons:
+                x.set_sensitive(True)
+        else:
+            for x in self.recurse_buttons:
+                x.set_sensitive(False)
+    def PC_onDestroy(self,x):
+        self.permissionChange.hide()
+
+
+def main():
+    applicaton = Application()
+
+    Gtk.main()
+
+if __name__ == "__main__":
+    main()
