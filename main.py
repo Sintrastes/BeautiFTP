6d9898ef10c2456b0abdf1857233f9cba704ad41
msmith533 <msmith533@liberty.edu>
2015-03-26 14:54:45 -0400

    Implemented File Chooser Cancel Handler

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ class Application:
     def BR_uploadHandler(self,x):
         self.filechooserdialog1.connect('delete-event', lambda w, e: w.hide() or True)
         self.filechooserdialog1.show_all()
-        
+
         #upload selected file to current working directory on server
         #self.server.storbinary("STOR filename")
 
@@ -209,7 +209,7 @@ class Application:
 #### TO DO
 #### File Chooser Window Handlers
     def FC_CancelHandler(self,x):
-        pass
+        self.filechooserdialog1.hide()
     def FC_OkHandler(self,x):
         pass
     def fileActivated(self,x):
